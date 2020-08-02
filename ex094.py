3b416661d816b02dcc634ee3d94bedb92b891607
Gabriel Wai Ramos dos Santos <62815808+gabrielwai@users.noreply.github.com>
2020-08-02 14:30:04 -0300

    Add files via upload

--- ex094.py
+++ ex094.py
@@ -17,17 +17,36 @@ while True:
         if continuar not in 'SsNs':
             print('Valor incorreto. Tente novamente.')
 
-    lista_de_pessoas.append(pessoa)
+    lista_de_pessoas.append(pessoa.copy())
 
     if continuar in 'Nn':
         break
 
-print(f'O grupo tem {len(lista_de_pessoas)} pessoas.')
+print('-='*30)
+print(lista_de_pessoas)
+print(f' - O grupo tem {len(lista_de_pessoas)} pessoas.')
 media_idade = 0
+mulheres = list()
+
 for c in range(len(lista_de_pessoas)):
     media_idade += lista_de_pessoas[c]['idade']
-print(f'A média de idade é de {media_idade} anos.')
-print('As mulheres cadastradas foram:',end=' \n')
+
+    if lista_de_pessoas[c]['sexo'] == 'F':
+        mulheres.append(lista_de_pessoas[c]['nome'])
+
+media_idade /= len(lista_de_pessoas)
+
+print(f' - A média de idade é de {media_idade:.2f} anos.')
+print(' - As mulheres cadastradas foram:', end=' ')
+for c in range(len(mulheres)-1):
+    print(mulheres[c], end=', ')
+print(mulheres[-1])
+
+print(' - Lista de pessoas com idade acima da média:', end='\n\n')
+chaves = list(lista_de_pessoas[0].keys())
 for c in range(len(lista_de_pessoas)):
-    if lista_de_pessoas[]
-    print(,end=' ')
+    if lista_de_pessoas[c]['idade'] > media_idade:
+        print(f'{chaves[0]} = {lista_de_pessoas[c]["nome"]}; '
+              f'{chaves[2]} = {lista_de_pessoas[c]["idade"]}; '
+              f'{chaves[1]} = {lista_de_pessoas[c]["sexo"]}')
+print('<< ENCERRADO >>')
